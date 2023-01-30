de69c9fa3ca4cd971f470096a1386d6931bacc2c
신성우 <ssw4510@gmail.com>
2023-01-30 19:17:15 +0900

    테일윈드css 텍스트 컨포넌트 생성 (클래스 적용 연습)

---
+++ Do_it_practice/ch03/ch03_2/src/App.tsx
@@ -0,0 +1,16 @@
+import TailwindcssTest from "./pages/TailwindcssTest"
+import Color from "./pages/Color"
+import TextsTest from "./pages/TextsTest"
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <TailwindcssTest />
+    </div>
+  )
+}
+
+export default App
