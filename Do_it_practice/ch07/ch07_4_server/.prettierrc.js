1a3b980bdf27c31b0686c6288d588b211e18dc0f
신성우 <ssw4510@gmail.com>
2023-02-24 02:19:44 +0900

    JSON웹 토큰 연습

---
+++ Do_it_practice/ch07/ch07_4_server/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQute: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  semi: false,
+  printWidth: 90
+}
