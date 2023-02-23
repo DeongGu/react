9af09ac6a4be3735b8440510e5677bbaff805dba
신성우 <ssw4510@gmail.com>
2023-02-23 19:18:04 +0900

    익스프레스로 api 서버 생성 연습

---
+++ Do_it_practice/ch07/ch07_3_client/src/components/Icon.tsx
@@ -0,0 +1,39 @@
+// import type {CSSProperties, FC} from "react"
+import type {FC, DetailedHTMLProps, HTMLAttributes} from "react"
+
+type ReactSpanProps = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+
+// export type IconProps = {
+//   name: string
+//   style?: CSSProperties
+// }
+
+export type IconProps = ReactSpanProps & {
+  name: string
+}
+
+// export const Icon: FC<IconProps> = ({name, style}) => {
+//   return (
+//     <span className="material-icons" style={style}>
+//       {name}
+//     </span>
+//   )
+// }
+
+export const Icon: FC<IconProps> = ({name, className: _className, ...props}) => {
+  const className = ["material-icons", _className].join(" ")
+
+  return (
+    <span {...props} className={className}>
+      {name}
+    </span>
+  )
+}
+
+// export const Icon: FC<IconProps> = ({name, ...props}) => {
+//   return (
+//     <span className="material-icons" {...props}>
+//       {name}
+//     </span>
+//   )
+// }
